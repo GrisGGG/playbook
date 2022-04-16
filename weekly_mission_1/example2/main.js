fb1218993eca027d59be8322a3ebd133fb62bb87
GrisGGG <astropoetscapicua@gmail.com>
2022-04-16 14:21:09 -0500

    Añadiendo apuntes

--- weekly_mission_1/example2/main.js
+++ weekly_mission_1/example2/main.js
@@ -1,6 +1,14 @@
+//Example 2: Exportando funciones entre scripts con CommonJS
+
 // node main.js
 
+/*Aquí hacemos que el archivo logger se introduzca a la variable logger con "require"
+así podemos usar objeots o partes del código dentro de este archivo
+*/
 const logger = require('./logger')
+
+//Mandamos a llamar a la función  dentro del archivo logger, llamando a la variable, punto y el nombre del obejto o función 
+//Para que estas funciones puedan ser llamadas, deben tener la palabra exports
 
 logger.info('This is an informational message')
 logger.verbose('This is a verbose message')
