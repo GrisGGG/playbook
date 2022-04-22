b1f541a2200f8d0e594da66985d7c528d22e5bf3
Guadalupe Gachupin <75553288+GrisGGG@users.noreply.github.com>
2022-04-22 13:56:11 -0500

    4. Borrando carpeta

--- weekly_mission_2/1Examples/example_1_1.js
+++
@@ -1,9 +0,0 @@
-//Ejemplo 2: Crear un objeto con propiedades
-
-const myObject2 ={
-    name: "Guadalupe",
-    age:29
-}
-
-console.log("Ejemplo 2: Crear un objeto con propiedades");
-console.log(myObject2);
