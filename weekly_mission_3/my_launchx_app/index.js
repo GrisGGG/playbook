aa733dfd4d3a7e7ebf5c6a9d26cfa69a8e731e73
GrisGGG <astropoetscapicua@gmail.com>
2022-04-26 19:24:43 -0500

    23. Añadiendo segundo proyecto

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo");
+const ferOchoa =new MissionCommander("Fernanda Ochoa");
+const rodrigo = new MissionCommander("Rodrigo")
+console.log(carlo.name);
+console.log(ferOchoa.name);
+console.log(rodrigo.name);
---
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -0,0 +1,6 @@
+describe("Test Suite Dummy Description", () => {
+    test ('Case 1 Dummy', () => {
+        const resultOfSomenthing = 1 + 2
+        expect(resultOfSomenthing).toBe(3);
+    });
+})
