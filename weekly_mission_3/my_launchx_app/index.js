7d779dbb9cd6544cb8577a2c26446c03e194704a
GrisGGG <astropoetscapicua@gmail.com>
2022-04-28 11:24:03 -0500

    24.Eliminando archivos para reubicarlos

--- weekly_mission_3/my_launchx_app/index.js
+++
@@ -1,7 +0,0 @@
-const MissionCommander = require('./app/missionCommander');
-const carlo = new MissionCommander("Carlo");
-const ferOchoa =new MissionCommander("Fernanda Ochoa");
-const rodrigo = new MissionCommander("Rodrigo")
-console.log(carlo.name);
-console.log(ferOchoa.name);
-console.log(rodrigo.name);
