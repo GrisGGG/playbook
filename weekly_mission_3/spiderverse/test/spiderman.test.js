5d342ccf44930e83364e2e7764a877c28c1e011d
GrisGGG <astropoetscapicua@gmail.com>
2022-04-28 11:38:23 -0500

    25. Elimando archivos spiderverse para reubicarlos

--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++
@@ -1,6 +0,0 @@
-describe("Test Suite Dummy Description", () => {
-    test ('Case 1 Dummy', () => {
-        const resultOfSomenthing = 1 + 2
-        expect(resultOfSomenthing).toBe(3);
-    });
-})
